b1be9d65de98da44875b0bb77135c70cccb85bba
Dorumin <dorminsama@gmail.com>
2019-04-30 02:19:13 -0300

    add css and remove useless double run protection

--- src/monkeypatch.js
+++ src/monkeypatch.js
@@ -5,37 +5,59 @@ const rimraf = require('rimraf');
 
 module.exports = (dir, extractPath) => {
     let window = fs.readFileSync(path.join(extractPath, 'app', 'mainScreen.js')).toString();
+    let js = fs
+        .readdirSync(path.join(path.dirname(__dirname), 'scripts'))
+        .map(file => {
+            return fs.readFileSync(path.join(path.dirname(__dirname), 'scripts', file));
+        })
+        .join('\n')
+        .replace(/\\/g, '\\\\')
+        .replace(/\`/g, '\\\`')
+        .replace(/\${/g, '\\${');
+    let css = fs
+        .readdirSync(path.join(path.dirname(__dirname), 'styles'))
+        .map(file => {
+            return fs.readFileSync(path.join(path.dirname(__dirname), 'styles', file));
+        })
+        .join('\n')
+        .replace(/\\/g, '\\\\')
+        .replace(/\`/g, '\\\`')
+        .replace(/\${/g, '\\${')
+        .replace(/\*(\w+)/g, '[class*="$1-"]');
 
-    if (!window.includes('autorun')) {
 
-        let js = fs
-            .readdirSync(path.join(process.cwd(), 'scripts'))
-            .map(file => {
-                return fs.readFileSync(path.join(process.cwd(), 'scripts', file));
-            })
-            .join('\n')
-            .replace(/\\/g, '\\\\')
-            .replace(/\`/g, '\\\`')
-            .replace(/\${/g, '\\${');
+    window = window.replace(/mainWindow.minimize[^}]+}/, `$&
 
-        window = window.replace(/mainWindow.minimize[^}]+}/, `$&
-
-            _electron.session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-                callback({
-                    responseHeaders: {
-                        ...details.responseHeaders,
-                        'content-security-policy': 'default-src *'
-                    },
-                });
+        _electron.session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+            callback({
+                responseHeaders: {
+                    ...details.responseHeaders,
+                    'content-security-policy': 'default-src *'
+                },
             });
-            
-            mainWindow.webContents.executeJavaScript('console.log("autorun?????");');
+        });
+        
+        mainWindow.webContents.executeJavaScript('console.log("autorun?????");');
 
-            mainWindow.webContents.executeJavaScript(\`${js}\`);
-        `);
+        mainWindow.webContents.executeJavaScript(\`${js}\`);
 
-        fs.writeFileSync(path.join(extractPath, 'app', 'mainScreen.js'), window);
-    }
+        mainWindow.webContents.executeJavaScript(\`console.log(\\\`${css}\\\`)\`);
+
+        mainWindow.webContents.executeJavaScript(\`
+            let style = document.createElement('style');
+            style.type = 'text/css';
+            style.rel = 'stylesheet';
+            document.head.appendChild(style);
+            style.textContent = \\\`${css}\\\`;
+            for (let rule of style.sheet.rules) {
+                if (!rule.selectorText.includes('#app-mount')) {
+                    rule.selectorText = '#app-mount ' + rule.selectorText
+                }
+            }
+        \`);
+    `);
+
+    fs.writeFileSync(path.join(extractPath, 'app', 'mainScreen.js'), window);
 
     asar.createPackage(extractPath, path.join(dir, 'modules', 'discord_desktop_core', 'core.asar')).then(() => {
         console.log('Patched!');
