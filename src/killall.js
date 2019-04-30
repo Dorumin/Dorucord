5dd376eee461ca0461804fc1875e171fc25dff2b
Dorumin <dorminsama@gmail.com>
2019-04-30 02:17:46 -0300

    Kill Discord processes before installing

---
+++ src/killall.js
@@ -0,0 +1,17 @@
+const ps = require('ps-node');
+
+module.exports = (processes) => {
+    if (!processes || !processes.length) return Promise.resolve();
+
+    return new Promise(res => {
+        let i = processes.length;
+        console.log(`Killing ${i} Discord processes...`)
+        processes.forEach(process => {
+            ps.kill(process.pid, () => {
+                console.log(`Killed process #${process.pid}!`)
+                if (--i) return;
+                res();
+            });
+        });
+    });
+}
