571cd18da5a0d49e870d2f36327bdadcabfaef4f
Dorumin <dorminsama@gmail.com>
2019-04-30 02:17:23 -0300

    Add some sass to installer.js

--- installer.js
+++ installer.js
@@ -1,14 +1,25 @@
 // Script I only made for Robyn that's too stupid to install BetterDiscord
-// She probably won't even be able to make it autorun when her device startups either
 const asar = require('asar');
+// She probably won't even be able to make it autorun when her device startups either
 const path = require('path');
-
 // Honestly I'm very doubtful she'll even read this before running it
+const ps = require('ps-node');
 // Or even install the necessary packages
 const getDir = require('./src/discorddir.js');
+// Or it'll probably not even run on macs in the first place
 const readTar = require('./src/readtar.js');
+// Why do I even bother doing these things for her, tbh
 const monkeyPatch = require('./src/monkeypatch.js');
+// Or maybe I'm pretending to be nice when I'm just doing this for myself
+const killAll = require('./src/killall.js');
 
-const extractPath = readTar(getDir());
-
-monkeyPatch(getDir(), extractPath);
+// Yeah, I'm too selfish for any of that, fuck being nice
+ps.lookup({
+    command: '.*app-\\d+.\\d+.\\d+.*discord.*'
+}, (_, processes) => {
+    killAll(processes).then(() => {
+        const extractPath = readTar(getDir());
+        
+        monkeyPatch(getDir(), extractPath);    
+    });
+});
