634c109db8df43bd42d92941b6cd50e479818407
Naughty <dorminsama@gmail.com>
2020-06-24 01:02:30 -0300

    danger

---
+++ scripts/danger.js
@@ -0,0 +1,11 @@
+(() => {
+    const _error = console.error;
+
+    console.error = (...args) => {
+        if (typeof args[0] === 'string' && args[0].includes('corrupt')) {
+
+        } else {
+            _error.apply(console, args);
+        }
+    }
+});
