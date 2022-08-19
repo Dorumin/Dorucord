97df7b651b6c441e196553626119f5ec86edf5e4
GameModerator <jeannelee823@gmail.com>
2022-08-19 14:08:36 +0800

    temporarily make it so core only builds the 'updated' ones

--- styles/core.js
+++ styles/core.js
@@ -3,9 +3,9 @@ const path = require('path');
 // I'm guessing BetterDorucord has a styles folder with a core folder
 const top = path.join(__dirname, 'core');
 const files = fs.readdirSync(top)
-  .filter(file => file.endsWith('.css'))
-  .map(file => path.join(top, file))
-  .map(path => fs.readFileSync(path).toString());
+    .filter(file => file.startsWith('!') && file.endsWith('.css'))
+    .map(file => path.join(top, file))
+    .map(path => fs.readFileSync(path).toString());
 
 // I don't know where you want this spit out
 fs.writeFileSync(path.join(__dirname, 'core.css'), files.join('\n\n'));
